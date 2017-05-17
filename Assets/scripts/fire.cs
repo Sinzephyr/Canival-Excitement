ee1dd15f06d318d0c8be406089df85d6660d8aa6
Sinzephyr <student@c515-d25hq0nldhjt-0072012.local>
2017-05-17 18:42:43 -0400

    problems with the fire mechanic

---
+++ Assets/scripts/fire.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fire : MonoBehaviour {
+
+
+	public float timer = 3f;
+	public float speed = 5f;
+	public Rigidbody projectile;
+
+	void start(){
+	}
+	void Update () { if (Input.GetKeyDown("space")) {
+		Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+		instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed)); }
+	}
+}
